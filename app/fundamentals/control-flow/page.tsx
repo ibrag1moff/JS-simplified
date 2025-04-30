67f5dbf980cff19463a8f6213d421f1c758e5e57
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-04-30 23:25:26 +0400

    Add Control Flow Chapter

---
+++ app/fundamentals/control-flow/page.tsx
@@ -0,0 +1,9 @@
+import SectionTitle from "@/widgets/SectionTitle";
+
+export default function ControlFlowPage() {
+  return (
+    <div className="flex flex-col gap-24 px-5">
+      <SectionTitle title="control flow" />
+    </div>
+  );
+}
