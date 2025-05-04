dfc5a542505ee480a7ce902964c36daf51936ef1
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-05-04 11:55:09 +0400

    Fix styles of 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] flex flex-col gap-4 text-center">
+    <div className="flex flex-col justify-center sm:mt-32 md:mt-40 gap-4 text-center">
       <h1 className="text-5xl sm:text-8xl text-primary">404</h1>
       <h1 className="text-3xl sm:text-5xl">PAGE NOT FOUND</h1>
       <Link
