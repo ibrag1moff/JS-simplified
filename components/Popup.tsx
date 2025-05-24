c87ca038fa7844d08af40cea5c80b5ff97fed8b8
Abbas Ibragimov <abbasibragimov123@gmail.com>
2025-05-24 17:37:45 +0400

    Debugging login

--- components/Popup.tsx
+++ components/Popup.tsx
@@ -37,7 +37,7 @@ export default function Popup() {
           <span className="text-primary ml-1">idempotent</span> 😄
         </p>
         <button
-          onClick={handleLogin}
+          onClick={handleGoogleLogin}
           className="bg-primary rounded-xl py-2 px-8 font-medium  cursor-pointer"
         >
           Login with Google
